7713c1a049ecd5485afe983fd4524df8a3938ddc
vaibhavalone32 <88182641+vaibhavalone32@users.noreply.github.com>
2021-07-30 16:19:17 +0530

    Update Second.java

--- GitTestPro/src/Second.java
+++ GitTestPro/src/Second.java
@@ -6,11 +6,17 @@ public class Second {
 		Second s = new Second();
 
 		s.m1();
+		s.m2();
 		System.out.println("github project second");
 	}
 
 	public void m1() {
 
 		System.out.println("m1 method");
+	}
+	
+	public void m2() {
+
+		System.out.println("m2 method");
 	}
 }
