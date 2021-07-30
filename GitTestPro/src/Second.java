0848b2b60d45e96d7771301de60cdf72e97697be
Chaitanya <Chaitanya@LAPTOP-K74000V7>
2021-07-30 15:32:55 +0530

    second git project by vaibhav

---
+++ GitTestPro/src/Second.java
@@ -0,0 +1,16 @@
+
+public class Second {
+
+	public static void main(String[] args) {
+
+		Second s = new Second();
+
+		s.m1();
+		System.out.println("github project second");
+	}
+
+	public void m1() {
+
+		System.out.println("m1 method");
+	}
+}
